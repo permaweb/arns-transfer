5890ab60d8b39c9ba6874a8cbe302a0990fcd37d
Justin Shaw <mogulx_operates@yahoo.com>
2023-05-31 16:57:53 -0500

    initial commit

---
+++ index.ts
@@ -0,0 +1,39 @@
+import fs from 'fs';
+
+import {
+  WarpFactory,
+  defaultCacheOptions,
+  LoggerFactory,
+} from 'warp-contracts';
+
+const ANT = 'TPvRqEeSctYC50c5mfP51nsbpGaCARtYO1fHyBZNLGE';
+const KEYFILE_PATH = process.env.PATH_TO_WALLET;
+const TARGET = process.env.ARNS_TARGET;
+
+(async function () {
+  LoggerFactory.INST.logLevel('error');
+
+  const warp = WarpFactory.forMainnet(
+    {
+      ...defaultCacheOptions,
+      inMemory: true,
+    },
+    true
+  );
+
+  const ownersWallet = JSON.parse(
+    fs.readFileSync(KEYFILE_PATH || '').toString()
+  );
+  const pst = warp.pst(ANT);
+  pst.connect(ownersWallet);
+  const interaction = await pst.transfer(
+    {
+      target: TARGET || '',
+      qty: 1,
+    },
+    {
+      disableBundling: true,
+    }
+  );
+  console.log(`Submitted transfer interaction: ${interaction!.originalTxId}`);
+})();
